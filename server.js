548fd0434072e8caf81dcdd394b4c650043d3b25
Sarthak Mallick <sarthak4242@gmail.com>
2025-02-04 21:57:32 -0500

    Assignment 2: added jest unit tests

--- server.js
+++ server.js
@@ -1,24 +1,6 @@
-const express = require("express");
-const sequelize = require("./config/database");
-const healthCheckRoutes = require("./routes/healthCheckRoutes");
-
-const app = express();
-app.use((req, res, next) => {
-  res.setHeader("Cache-Control", "no-cache");
-  if (req.method != "GET") {
-    res.status(405).end()
-  }
-  next();
-});
-app.use(express.json());
-app.use("/", healthCheckRoutes);
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
-sequelize.sync().then(() => {
-    console.log("Database sync successful");
-    app.listen(port, () => {
-      console.log("Server started on http://localhost:"+port);
-    });
-  }).catch((error) => {
-    console.error("Database sync failed: ", error);
-  });
+app.listen(port, () => {
+  console.log("Server started on http://localhost:" + port);
+});
