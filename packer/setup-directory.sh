6798bf13e778b2d0e1d2ddd7c577c75da418d636
Sarthak Mallick <sarthak4242@gmail.com>
2025-02-26 03:02:23 -0500

    removed debugging echo

--- packer/setup-directory.sh
+++ packer/setup-directory.sh
@@ -5,13 +5,10 @@ set -e
 export DEBIAN_FRONTEND=noninteractive
 export CHECKPOINT_DISABLE=1
 
-echo "Contents of /tmp/webapp before copy"
-ls -la /tmp/webapp
+# copy from /tmp/opt/webapp to /opt/csye6225/webapp
 sudo mkdir -p /opt/csye6225/webapp
 sudo chmod -R 755 /opt/csye6225/webapp
 sudo cp -R /tmp/webapp/* /opt/csye6225/webapp/
-echo "Contents of opt/csye6225/webapp after copy"
-ls -la /opt/csye6225/webapp
 
 # create environment variable file
 sudo touch /opt/csye6225/webapp/.env
@@ -24,7 +21,6 @@ sudo chmod 666 /opt/csye6225/webapp/.env
   echo "PORT=${PORT}"
 } > "/opt/csye6225/webapp/.env"
 
-cat /opt/csye6225/webapp/.env
 # install node modules
 cd /opt/csye6225/webapp || exit
 sudo npm install
