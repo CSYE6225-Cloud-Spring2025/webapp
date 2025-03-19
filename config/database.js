c8250efdc17594c2b5847fa56daf4e237ec8e1c0
Sarthak Mallick <sarthak4242@gmail.com>
2025-03-18 23:19:27 -0400

    port for db connection

--- config/database.js
+++ config/database.js
@@ -10,7 +10,8 @@ if (missingEnvVars.length > 0) {
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: "mysql",
+  port: 3306,
+  dialect: "mysql"
 });
 
 sequelize.authenticate().then(() => {
