c7791880be83f7bb9887fc1edfe02744f6470095
Sarthak Mallick <sarthak4242@gmail.com>
2025-03-25 19:54:45 -0400

    permission for github runner

--- packer/setup-user.sh
+++ packer/setup-user.sh
@@ -9,14 +9,14 @@ export CHECKPOINT_DISABLE=1
 sudo groupadd "$APP_GROUP"
 sudo useradd --system -m -g "$APP_GROUP" -s /usr/sbin/nologin "$APP_USER"
 
-# provide app folder permissions to the user and group
+# provide app directory permissions to the user and group
 sudo chown -R "$APP_USER":"$APP_GROUP" /opt/csye6225
 sudo chmod -R 750 /opt/csye6225
 
-# provide log folder permissions to the user and group
+# provide log directory permissions to the user and group
 sudo chown -R "$APP_USER":"$APP_GROUP" /var/log/nodejs
 sudo chmod -R 750 /var/log/nodejs
 
-# provide cloudwatch config folder permissions to the user and group
+# provide cloudwatch config directory permissions to the user and group
 sudo chown -R "$APP_USER":"$APP_GROUP" /opt/aws/amazon-cloudwatch-agent/etc/
 sudo chmod -R 750 /opt/aws/amazon-cloudwatch-agent/etc/
