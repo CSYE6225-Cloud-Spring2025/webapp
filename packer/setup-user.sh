2eeae3cf79f8d620a7cbd800f45eb7a31ee4db76
Sarthak Mallick <sarthak4242@gmail.com>
2025-03-25 19:43:37 -0400

    permission to acccess cloudwatch config

--- packer/setup-user.sh
+++ packer/setup-user.sh
@@ -16,3 +16,7 @@ sudo chmod -R 750 /opt/csye6225
 # provide log folder permissions to the user and group
 sudo chown -R "$APP_USER":"$APP_GROUP" /var/log/nodejs
 sudo chmod -R 750 /var/log/nodejs
+
+# provide cloudwatch config folder permissions to the user and group
+sudo chown -R "$APP_USER":"$APP_GROUP" /opt/aws/amazon-cloudwatch-agent/etc/
+sudo chmod -R 750 /opt/aws/amazon-cloudwatch-agent/etc/
